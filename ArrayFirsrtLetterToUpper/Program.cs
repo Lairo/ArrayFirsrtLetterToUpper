745da57d6a15690c4e01b26a6ada3546a3087e58
chajus1 <chajus1@googlemail.com>
2023-02-01 15:02:08 +0000

    Add project files.

---
+++ ArrayFirsrtLetterToUpper/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayFirsrtLetterToUpper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friends = new string[5] { "bon", "don", "ron", "shaun", "mike" };
+
+            int counter = 1;
+            foreach (string friend in friends)
+            {
+                Console.WriteLine($"\tFriend Nr.{counter++}\n \t{char.ToUpper(friend[0]) + friend.Substring(1)} \n");
+            }
+        }
+    }
+}
